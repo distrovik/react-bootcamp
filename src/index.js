33dd2d3bd0948e6bcb037ddc24db8193509b75e2
Mr. Irfan <mrirfanwork@gmail.com>
2023-07-30 19:30:13 -0700

    JSX introduction

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <div>
+      <h1>My Favourite Foods</h1>
+      <ul>
+        <li>Burger</li>
+        <li>Ramen</li>
+        <li>Wings</li>
+      </ul>
+    </div>
+  </React.StrictMode>
+);
