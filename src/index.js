38c26644d2870e26844d67ef425b9bfa68bc7374
Mr. Irfan <mrirfanwork@gmail.com>
2023-07-30 22:06:32 -0700

    JS Expressions in JSX & ES6 Template Literals

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+const name = "VIX";
+const time = new Date();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div>
-      <h1>My Favourite Foods</h1>
-      <ul>
-        <li>Burger</li>
-        <li>Ramen</li>
-        <li>Wings</li>
-      </ul>
+  <div>
+    <h1>Hello {`MISTA`} {`${name}`}!</h1>
+    <p>Your local time is {time.getHours()} {": " + time.getMinutes()}</p>
     </div>
   </React.StrictMode>
 );
